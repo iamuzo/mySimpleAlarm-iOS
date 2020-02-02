7d4110e02d7e8e7b18257192240b55ef1d220265
IamUzo <iamuzoagu@gmail.com>
2020-02-01 19:38:20 -0700

    adds ability to: - schedule local notifications using a Custom Protocol and Extension - cancel local notifications via same Custom Protocol and Extension

--- mySimpleAlarm/ModelControllers/AlarmController.swift
+++ mySimpleAlarm/ModelControllers/AlarmController.swift
@@ -7,8 +7,49 @@
 //
 
 import Foundation
+import UserNotifications
 
-class AlarmController {
+protocol AlarmScheduler {
+    func scheduleUserNotifications(for alarm: Alarm)
+    func cancelUserNotifications(for alarm: Alarm)
+}
+
+extension AlarmScheduler {
+    //default implementaions for 2 protocol functions above
+    func scheduleUserNotifications(for alarm: Alarm) {
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Time's up!"
+        notificationContent.body = "Alarm: \(alarm.title) has gone off"
+        notificationContent.sound = UNNotificationSound.default
+        
+        let startTime = alarm.startTime
+        let triggerDate = Calendar.current.dateComponents(
+            [.hour, .minute, .second], from: startTime
+        )
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: triggerDate,
+            repeats: true
+        )
+        
+        let request = UNNotificationRequest(
+            identifier: alarm.uuid,
+            content: notificationContent,
+            trigger: trigger
+        )
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("notification error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func cancelUserNotifications(for alarm: Alarm) {
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [alarm.uuid])
+    }
+}
+
+class AlarmController: AlarmScheduler {
     
     //MARK:- Global Shared Instance
     static let sharedGlobalInstance = AlarmController()
@@ -24,19 +65,27 @@ class AlarmController {
     func create(newAlarmWith title: String, isEnabled: Bool = true, startTime: Date) {
         let alarm = Alarm(title: title, isEnabled: isEnabled, startTime: startTime)
         alarms.append(alarm)
+        scheduleUserNotifications(for: alarm)
         saveToLocalPersistenceStore()
     }
 
-    func update(existingAlarm alarm: Alarm, withNewTitle title: String, newIsEnabled isEnabled: Bool, andNewStartTime startTime: Date) {
+    func update(existingAlarm alarm: Alarm,
+                withNewTitle title: String,
+                newIsEnabled isEnabled: Bool,
+                andNewStartTime startTime: Date) {
+        
         guard let indexOfAlarmToUpdate = alarms.firstIndex(of: alarm) else {
             print("Cannot find an alarm at the index of the editedAlarm")
             return
         }
+        
         let alarmToUpdate = alarms[indexOfAlarmToUpdate]
+        
         if alarmToUpdate == alarm {
             alarmToUpdate.title = title
             alarmToUpdate.isEnabled = isEnabled
             alarmToUpdate.startTime = startTime
+            scheduleUserNotifications(for: alarmToUpdate)
             saveToLocalPersistenceStore()
         } else {
             print("The two objects are not the same")
@@ -46,6 +95,7 @@ class AlarmController {
     func delete(existingAlarm alarm: Alarm) {
         if let index = alarms.firstIndex(of: alarm) {
             alarms.remove(at: index)
+            cancelUserNotifications(for: alarm)
             saveToLocalPersistenceStore()
         }
     }
